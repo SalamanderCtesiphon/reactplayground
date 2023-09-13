6eef9cdd37578c00e81f6a5e4615d850d6f39eb5
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-13 12:17:07 -0500

    created two new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Greeting from './components/Greeting'
+import { FavoriteFood } from './components/MoreComponents'
+import { FavoriteDrink } from './components/MoreComponents'
 
 function App() {
 
   return (
     <>
       <Greeting />
+      <FavoriteFood />
+      <FavoriteDrink />
     </>
   )
 }
---
+++ src/components/MoreComponents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function FavoriteFood() {
+  return (
+    <div>FavoriteFood</div>
+  )
+}
+
+export function FavoriteDrink() {
+  return (
+    <div>FavoriteDrink</div>
+  )
+}
