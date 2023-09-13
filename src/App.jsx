1d82c207c534fa87afd637d32acbc678df77229c
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-13 16:03:41 -0500

    created a button component and practiced passing props to it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Greeting from './components/Greeting'
 import { FavoriteFood } from './components/MoreComponents'
 import { FavoriteDrink } from './components/MoreComponents'
+import Button from './components/Button'
 
 function App() {
 
@@ -10,6 +11,9 @@ function App() {
       <Greeting />
       <FavoriteFood />
       <FavoriteDrink />
+      <Button text="Click me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click me!" color="blue" fontSize={20} />
     </>
   )
 }
