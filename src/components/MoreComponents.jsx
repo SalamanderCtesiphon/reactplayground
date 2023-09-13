79c2531d4a24eacaff6e57f2a8ea6f921ed4ed69
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-13 12:52:54 -0500

    added a new element to one of my components

--- src/components/MoreComponents.jsx
+++ src/components/MoreComponents.jsx
@@ -8,6 +8,22 @@ export function FavoriteFood() {
 
 export function FavoriteDrink() {
   return (
-    <div>FavoriteDrink</div>
+    <div>
+      <h1>FavoriteDrink</h1>
+      <div>
+        <h1>Test title</h1>
+        <ol className="test-list">
+          <li>List item 1</li>
+          <li>List item 2</li>
+          <li>List item 3</li>
+        </ol>
+        <svg>
+          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+        </svg>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    </div>
   )
 }
