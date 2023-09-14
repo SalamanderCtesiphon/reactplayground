56e695f9c946de2c43f1f590d8413f6a5a09408d
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-14 13:04:50 -0500

    changed the list element to only render animal that start with L

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -8,7 +8,7 @@ function List(props) {
   return(
     <ul>
       {props.animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />
+        return animal.startsWith("L") ? <ListItem key={animal} animal={animal} /> : null
       })}
     </ul>
   )
