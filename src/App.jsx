5c491ec47f57e6d34ca8ffe23b20e15b453c1145
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-22 13:02:48 -0500

    I made a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt='Kat in a hat'
+    />
+  )
+}
+
 function App() {
 
   
 
   return (
     <>
+      <section>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
       
     </>
   )
