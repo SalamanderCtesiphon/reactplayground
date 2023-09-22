9902067bbf85f97a09353e44d87256b875841f8a
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-22 12:46:45 -0500

    deleted some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
-import Greeting from './components/Greeting'
-import { FavoriteFood } from './components/MoreComponents'
-import { FavoriteDrink } from './components/MoreComponents'
-import Button from './components/Button'
-import Animal from './components/Animal'
 
 function App() {
 
-  const handleButtonClick = () => {
-    window.location.href = "http://www.google.com"
   }
 
   return (
     <>
-      <Greeting />
-      <FavoriteFood />
-      <FavoriteDrink />
-      <Button handleClick={handleButtonClick} />
-      <Button text="Don't Click Me!" color="red" fontSize={12} />
-      <Button fontSize={20} />
-      <Animal />
+      
     </>
   )
 }
